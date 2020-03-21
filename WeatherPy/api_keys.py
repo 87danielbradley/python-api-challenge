150bfaf0b06bc3f5a561ce9fac3dfc2553085e2d
Daniel Bradley <87danielbradley@gmail.com>
2020-03-21 10:08:15 -0400

    test

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "47fc7da5c4965fa5b5626acae7bd8302"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyA4njJE_w2_0m2gpU6858xdd4JhWd84YFs"
